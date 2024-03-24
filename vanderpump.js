41f75eb4bcc5fdb098c62c94e60b4e9ad04583d3
Sofia Magnani <sofiamagnani@Sofias-Air.lan>
2024-03-24 14:44:01 -0700

    add website

---
+++ vanderpump.js
@@ -0,0 +1,11 @@
+let x = document.getElementById("myDIV");
+let y = document.getElementById('lisaPhoto');
+
+
+function myFunction() {
+    document.getElementById("myDIV").innerHTML= "Lisa Vanderpump is the queen of the castle. The boss lady. ";
+    let image = document.getElementById("lisaPhoto");
+    image.src = "https://imageio.forbes.com/specials-images/imageserve/63e2c85419d9f74be141447d/Lisa-Vanderpump/0x0.jpg?format=jpg&crop=750,423,x0,y97,safe&width=1440"
+    document.getElementById("btnIDLisa").style.display='none';
+}
+  
